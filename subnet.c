630f4b6c0dc5306473dfea76cb3bc1f9b922dbab
Christopher Price <chris@pricedude.com>
2023-08-12 10:59:24 -0400

    Completed the parseCIDR function

--- subnet.c
+++ subnet.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <regex.h>
 #include <stdbool.h>
+#include <string.h>
 
 int match(const char *string, const char *pattern);
+void parseCIDR(char *string, int *ip, int *cidr);
 
 int main(int argc, char *argv[]) {
     bool inputValidation = false;
@@ -22,7 +25,10 @@ int main(int argc, char *argv[]) {
     // they conform to the correct dotted decimal (IP MASK) notation.
     if (argc == 2) {
         if (match(argv[1], cidrValidation)) {
+            parseCIDR(argv[1], ip, &cidr);
             inputValidation = true;
+
+            printf("%d.%d.%d.%d/%d\n", ip[0], ip[1], ip[2], ip[3], cidr);
         }
     } else if (argc == 3) {
         if ( (match(argv[1], ddnValidation)) && (match(argv[2], ddnValidation))) {
@@ -48,4 +54,32 @@ int match(const char *string, const char *pattern) {
     regfree(&re);
     if (status != 0) return 0;
     return 1;
+}
+
+void parseCIDR(char *string, int *ip, int *cidr) {
+    int i = 0;
+
+    // First, seperate the IP from the CIDR parts of the string
+    char delim1[] = "/";
+    char *ptr1 = strtok(string, delim1);
+
+    i = 0;
+    while(ptr1 != NULL) {
+        if (i==1) { *cidr = atoi(ptr1); }
+        i++;
+		ptr1 = strtok(NULL, delim1);
+	}
+
+    // Next, parse out the ip octets
+    char a[4], b[4], c[4], d[4], delim2[] = ".";
+    char *ptr2 = strtok(string, delim2);
+    i = 0;
+    while(ptr2 != NULL) {
+        if (i==0) { ip[0] = atoi(ptr2); }
+        if (i==1) { ip[1] = atoi(ptr2); }
+        if (i==2) { ip[2] = atoi(ptr2); }
+        if (i==3) { ip[3] = atoi(ptr2); }
+        i++;
+		ptr2 = strtok(NULL, delim2);
+	}
 }
