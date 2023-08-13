246ca5d9681c08d4d74466ca3c22b8ba52aa73c6
Christopher Price <chris@pricedude.com>
2023-08-12 21:31:31 -0400

    Ended for the night...trying to create cidr to binay

--- subnet.c
+++ subnet.c
@@ -7,6 +7,10 @@
 // Prototypes
 int match(const char *string, const char *pattern);
 void parseCIDR(char *string, int *ip, int *cidr);
+void parseDDN(char *string, int *ip);
+int validateIP(int *ip);
+int validateMask(int *mask);
+void cidr_to_mask(int cidr, int *mask);
 
 int main(int argc, char *argv[]) {
     bool inputValidation = false;
@@ -26,14 +30,19 @@ int main(int argc, char *argv[]) {
     // they conform to the correct dotted decimal (IP MASK) notation.
     if (argc == 2) {
         if (match(argv[1], cidrValidation)) {
-            parseCIDR(argv[1], ip, &cidr);
             inputValidation = true;
-
-            printf("%d.%d.%d.%d/%d\n", ip[0], ip[1], ip[2], ip[3], cidr);
+            parseCIDR(argv[1], ip, &cidr);
+            if (!validateIP(ip)) { inputValidation = false; }
+            if ( (cidr < 1) || (cidr > 32) ) { inputValidation = false; }
+            if (inputValidation == true) { cidr_to_mask(cidr, mask); }
         }
     } else if (argc == 3) {
         if ( (match(argv[1], ddnValidation)) && (match(argv[2], ddnValidation))) {
             inputValidation = true;
+            parseDDN(argv[1], ip);
+            parseDDN(argv[2], mask);
+            if (!validateIP(ip)) { inputValidation = false; }
+            if (!validateMask(mask)) { inputValidation = false; }
         }
     }
 
@@ -48,6 +57,10 @@ int main(int argc, char *argv[]) {
 }
 
 int match(const char *string, const char *pattern) {
+    // takes a string and compares it with a passed regular expression.
+    // if there is a match, it returns 1, and if there is no match it
+    // returns 0.
+
     regex_t re;
     if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) return 0;
     int status = regexec(&re, string, 0, NULL, 0);
@@ -57,6 +70,12 @@ int match(const char *string, const char *pattern) {
 }
 
 void parseCIDR(char *string, int *ip, int *cidr) {
+    // Takes in a string representation of IP/CIDR and first splits it
+    // into a string representation of an IP address, and a string
+    // representation of a CIDR notation. It converts the CIDR notation
+    // into the cidr int, then converts the string representation of the
+    // IP into an ip int array.
+
     int i = 0;
 
     // First, seperate the IP from the CIDR parts of the string
@@ -71,15 +90,55 @@ void parseCIDR(char *string, int *ip, int *cidr) {
 	}
 
     // Next, parse out the ip octets
-    char a[4], b[4], c[4], d[4], delim2[] = ".";
-    char *ptr2 = strtok(string, delim2);
-    i = 0;
-    while(ptr2 != NULL) {
-        if (i==0) { ip[0] = atoi(ptr2); }
-        if (i==1) { ip[1] = atoi(ptr2); }
-        if (i==2) { ip[2] = atoi(ptr2); }
-        if (i==3) { ip[3] = atoi(ptr2); }
+    parseDDN(string, ip);
+}
+
+void parseDDN(char *string, int *ip) {
+    // Takes in a string representation of an IP address, splts it into
+    // 4 octets using the . as a delimeter, then casts each string represented
+    // octet into an IP int array.
+
+    char a[4], b[4], c[4], d[4], delim[] = ".";
+    char *ptr = strtok(string, delim);
+    int i = 0;
+    while(ptr != NULL) {
+        if (i==0) { ip[0] = atoi(ptr); }
+        if (i==1) { ip[1] = atoi(ptr); }
+        if (i==2) { ip[2] = atoi(ptr); }
+        if (i==3) { ip[3] = atoi(ptr); }
         i++;
-		ptr2 = strtok(NULL, delim2);
+		ptr = strtok(NULL, delim);
 	}
+}
+
+int validateIP(int *ip) {
+    // Validates an IPv4 address from 1.0.0.0 thru 223.255.255.255
+
+    if (  (ip[0] >= 1) && (ip[0] <= 223) &&
+        ( (ip[1] >= 0) && (ip[1] <= 255) ) &&
+        ( (ip[2] >= 0) && (ip[2] <= 255) ) &&
+        ( (ip[3] >= 0) && (ip[3] <= 255) ) )
+    {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int validateMask(int *mask) {
+    // Validates IPv4 maskbinMask[i]
+
+    if (  (mask[0] >= 0) && (mask[0] <= 255) &&
+        ( (mask[1] >= 0) && (mask[1] <= 255) ) &&
+        ( (mask[2] >= 0) && (mask[2] <= 255) ) &&
+        ( (mask[3] >= 0) && (mask[3] <= 255) ) )
+    {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void cidr_to_mask(int cidr, int *mask) {
+
 }
